79b9df2158cf2cf0d5a44b2ccc93fb37ec0a20c1
talshbt <talshbt@gmail.com>
2020-07-31 12:29:58 +0300

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -14,55 +14,6 @@ export class AppComponent {
     private snackBar: MatSnackBar) {
   }
 
-//   public constructDialog<T>(TCtor: new (...args: any[]) => T,data: any): MatDialogRef<T,any> {
-//     const dialogConfig = new MatDialogConfig();
-//     dialogConfig.autoFocus = true;
-//     const dialogRef = this.dialog.open(TCtor, dialogConfig);
-//     return dialogRef;
-//  }
-
-//  openGenericDialog() {
-//    const dialogRef = this.constructDialog(ConfirmationDialog,{
-//       width:'1000px',
-//       height:'400px',
-//            data:{
-//         message: 'Are you sure want to delete?',
-//         buttonText: {
-//           ok: 'Save',
-//           cancel: 'No'
-//         }
-//       }
-//    });
-//  }
-
-//   openDialog() {
-//     const dialogRef = this.dialog.open(ConfirmationDialog,{
-//       width:'1000px',
-//       height:'400px',
-//       data:{
-//         message: 'Are you sure want to delete?',
-//         arr: this.arr,
-//         buttonText: {
-//           ok: 'Save',
-//           cancel: 'No'
-//         }
-//       }
-//     });
-//     const snack = this.snackBar.open('Snack bar open before dialog');
-
-//     dialogRef.afterClosed().subscribe((confirmed: boolean) => {
-//       if (confirmed) {
-//         snack.dismiss();
-//         const a = document.createElement('a');
-//         a.click();
-//         a.remove();
-//         snack.dismiss();
-//         this.snackBar.open('Closing snack bar in a few seconds', 'Fechar', {
-//           duration: 2000,
-//         });
-//       }
-//     });
-//   }
 
   openAlertDialog() {
     const dialogRef = this.dialog.open(AlertDialogComponent,{
@@ -78,21 +29,3 @@ export class AppComponent {
 
 
 
-/**
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-
-// var dates = []
-
-//     var now = moment().startOf('day').hour(10).minute(30).seconds(0)
-//     var deadline = moment().hour(19).minute(0).seconds(0)
-//     //dates.push(now.format('HH:mm A'))
-//     while (now.diff(deadline) < 0) {
-//       if (now >= moment(now).hour(10)) {
-//         dates.push(now.format('hh:mm A'))
-//       }
-//       now.add(12, 'minutes');
-//     }
